391617796ba36aecb45a7443f7aa6ab9f3df115b
giorgosdejonge <giorgos.dejonge@student.maastrichtuniversity.nl>
2024-10-15 16:44:18 +0200

    insert order dessert and drink

--- orderinsertlogic.py
+++ orderinsertlogic.py
@@ -8,14 +8,9 @@ engine = create_engine('mysql+pymysql://root:toolbox@localhost/PizzaShop', echo=
 Session = sessionmaker(bind=engine)
 
 class orderInsertLogic:
-
-
     @staticmethod
     def insert_order(
-        username: str, order_total: float, postal_code: str,
-        pizza_id: int, order_id: int, pizza_amount: int,
-        drink_id: int, drink_amount: int,
-        dessert_id: int, dessert_amount: int) -> bool:
+        username: str, order_total: float, postal_code: str,) -> bool:
         session = Session()  
         
         try:
@@ -31,26 +26,85 @@ class orderInsertLogic:
                 postal_code=postal_code,
                 order_datetime=datetime.now() 
             ))
-
-            orderpizza = session.execute(
-                select(OrderPizza).where(OrderPizza.pizza_id == pizza_id)
-            ).fetchone()
-
-            if not orderpizza:
-                return False
-            session.execute(insert(OrderPizza).values(
-                order_id = order_id,
-                pizza_id = pizza_id,
-                pizza_amount = pizza_amount
-            ))
-
-
             session.commit()
             return True 
         
-            
-            
+        except Exception:
+            session.rollback()  
+            return False  
 
+        finally:
+            session.close()
+
+    def insert_order_pizza(order_id: int, pizza_id: int, pizza_amount: int) -> bool:
+        session = Session()
+
+        try:
+            orderpizza = session.execute(
+                select(OrderPizza).where(OrderPizza.order_id == order_id)
+            ).fetchone()
+
+            if not orderpizza:
+                return False  
+            session.execute(insert(OrderPizza).values(
+                pizza_id = pizza_id,
+                order_id = order_id,
+                pizza_amount = pizza_amount
+            ))
+            session.commit()
+            return True 
+    
+        except Exception:
+            session.rollback()  
+            return False  
+
+        finally:
+            session.close()
+
+        
+    def insert_order_drink(order_id: int, drink_id: int, drink_amount: int) -> bool:
+        session = Session()
+
+        try:
+            orderdrink = session.execute(
+                select(OrderDrink).where(OrderDrink.order_id == order_id)
+            ).fetchone()
+
+            if not orderdrink:
+                return False  
+            session.execute(insert(OrderDrink).values(
+                drink_id = drink_id,
+                order_id = order_id,
+                drink_amount = drink_amount
+            ))
+            session.commit()
+            return True 
+    
+        except Exception:
+            session.rollback()  
+            return False  
+
+        finally:
+            session.close()
+
+    def insert_order_dessert(order_id: int, dessert_id: int, dessert_amount: int) -> bool:    
+        session = Session()
+
+        try:
+            orderdessert = session.execute(
+                select(OrderDessert).where(OrderDessert.order_id == order_id)
+            ).fetchone()
+
+            if not orderdessert:
+                return False  
+            session.execute(insert(OrderDessert).values(
+                dessert_id = dessert_id,
+                order_id = order_id,
+                dessert_amount = dessert_amount
+            ))
+            session.commit()
+            return True 
+    
         except Exception:
             session.rollback()  
             return False  
